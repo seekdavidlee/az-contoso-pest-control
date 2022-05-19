a2368f828e317e5991341614378b1d3bbae5ed66
David Lee <leedavid@microsoft.com>
2022-05-19 10:33:40 -0500

    testing some code

--- src/ContosoPestControl.Issue/Program.cs
+++ src/ContosoPestControl.Issue/Program.cs
@@ -82,11 +82,11 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
